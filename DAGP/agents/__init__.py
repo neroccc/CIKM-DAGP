25e740893ed2820d59683e401f75a0bbaf54ef7d
neroccc <tomaswang199611@gmail.com>
2025-08-21 19:15:27 -0700

    Update __init__.py

--- DAGP/agents/__init__.py
+++ DAGP/agents/__init__.py
@@ -1,10 +1,10 @@
-from AgentDropout.agents.analyze_agent import AnalyzeAgent
-from AgentDropout.agents.code_writing import CodeWriting
-from AgentDropout.agents.math_solver import MathSolver
-from AgentDropout.agents.math_solver_aqua import MathSolver_aqua
-from AgentDropout.agents.adversarial_agent import AdverarialAgent
-from AgentDropout.agents.final_decision import FinalRefer,FinalDirect,FinalWriteCode,FinalMajorVote
-from AgentDropout.agents.agent_registry import AgentRegistry
+from DAGP.agents.analyze_agent import AnalyzeAgent
+from DAGP.agents.code_writing import CodeWriting
+from DAGP.agents.math_solver import MathSolver
+from DAGP.agents.math_solver_aqua import MathSolver_aqua
+from DAGP.agents.adversarial_agent import AdverarialAgent
+from DAGP.agents.final_decision import FinalRefer,FinalDirect,FinalWriteCode,FinalMajorVote
+from DAGP.agents.agent_registry import AgentRegistry
 
 __all__ =  ['AnalyzeAgent',
             'CodeWriting',
