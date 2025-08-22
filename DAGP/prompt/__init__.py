ecd3da9c4e89d67bc749288eedf1a1688088c91e
neroccc <tomaswang199611@gmail.com>
2025-08-21 19:16:22 -0700

    Update __init__.py

--- DAGP/prompt/__init__.py
+++ DAGP/prompt/__init__.py
@@ -1,10 +1,10 @@
-from AgentDropout.prompt.prompt_set_registry import PromptSetRegistry
-from AgentDropout.prompt.mmlu_prompt_set import MMLUPromptSet
-from AgentDropout.prompt.humaneval_prompt_set import HumanEvalPromptSet
-from AgentDropout.prompt.gsm8k_prompt_set import GSM8KPromptSet
-from AgentDropout.prompt.aqua_prompt_set import AQUAPromptSet
-from AgentDropout.prompt.math_prompt_set import MathPromptSet
-from AgentDropout.prompt.mathc_prompt_set import MathcPromptSet
+from DAGP.prompt.prompt_set_registry import PromptSetRegistry
+from DAGP.prompt.mmlu_prompt_set import MMLUPromptSet
+from DAGP.prompt.humaneval_prompt_set import HumanEvalPromptSet
+from DAGP.prompt.gsm8k_prompt_set import GSM8KPromptSet
+from DAGP.prompt.aqua_prompt_set import AQUAPromptSet
+from DAGP.prompt.math_prompt_set import MathPromptSet
+from DAGP.prompt.mathc_prompt_set import MathcPromptSet
 
 __all__ = ['MMLUPromptSet',
            'HumanEvalPromptSet',
